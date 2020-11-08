8cb1579db514ba04c1dd608050c0fc5dc0fba204
Joshua Huang <johuang50@gmail.com>
2020-11-08 14:06:24 -0500

    Add bridge mode toggle and functioning duplicator

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,91 @@
+import org.jnativehook.GlobalScreen;
+import org.jnativehook.NativeHookException;
+import org.jnativehook.keyboard.NativeKeyEvent;
+import org.jnativehook.keyboard.NativeKeyListener;
 import org.jnativehook.mouse.NativeMouseEvent;
 import org.jnativehook.mouse.NativeMouseInputListener;
 
-import javax.swing.*;
 import java.awt.*;
+import java.awt.event.InputEvent;
 
-public class Main implements NativeMouseInputListener {
+public class Main implements NativeMouseInputListener, NativeKeyListener {
+
+    Robot bot;
+    boolean bridgeMode = false;
+
+    {
+        try {
+            bot = new Robot();
+        } catch (AWTException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void nativeMouseClicked(NativeMouseEvent e) {
+
+    }
+
+    public void nativeMousePressed(NativeMouseEvent e) {
+        System.out.println("Mouse Pressed: " + e.getButton());
+        if (e.getButton() == 2) {
+            for (int i = 0; i < (bridgeMode ? 2 : 1); i++) {
+                bot.mousePress(InputEvent.BUTTON2_DOWN_MASK);
+//                bot.mouseRelease(InputEvent.BUTTON2_DOWN_MASK);
+                try {
+                    Thread.sleep(30);
+                } catch (InterruptedException interruptedException) {
+                    interruptedException.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public void nativeMouseReleased(NativeMouseEvent e) {
+        if (e.getButton() == 2) {
+            bot.mouseRelease(InputEvent.BUTTON2_DOWN_MASK);
+        }
+    }
+
+    public void nativeMouseMoved(NativeMouseEvent e) {
+    }
+
+    public void nativeMouseDragged(NativeMouseEvent e) {
+    }
+
     public static void main(String[] args) {
-        Toolkit.getDefaultToolkit().addAWTEventListener(event -> System.out.println("MOUSE BUTTON PRESSED"), AWTEvent.MOUSE_EVENT_MASK | AWTEvent.FOCUS_EVENT_MASK);
-        JFrame frame = new JFrame();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
+        try {
+            GlobalScreen.registerNativeHook();
+        }
+        catch (NativeHookException ex) {
+//            System.err.println("There was a problem registering the native hook.");
+//            System.err.println(ex.getMessage());
+
+            System.exit(1);
+        }
+
+        // Construct the example object.
+        Main example = new Main();
+
+        // Add the appropriate listeners.
+        GlobalScreen.addNativeMouseListener(example);
+        GlobalScreen.addNativeKeyListener(example);
     }
 
     @Override
-    public void nativeMouseClicked(NativeMouseEvent nativeMouseEvent) {
-        
-    }
-
-    @Override
-    public void nativeMousePressed(NativeMouseEvent nativeMouseEvent) {
+    public void nativeKeyTyped(NativeKeyEvent nativeKeyEvent) {
 
     }
 
     @Override
-    public void nativeMouseReleased(NativeMouseEvent nativeMouseEvent) {
+    public void nativeKeyPressed(NativeKeyEvent nativeKeyEvent) {
 
     }
 
     @Override
-    public void nativeMouseMoved(NativeMouseEvent nativeMouseEvent) {
-
-    }
-
-    @Override
-    public void nativeMouseDragged(NativeMouseEvent nativeMouseEvent) {
-
+    public void nativeKeyReleased(NativeKeyEvent nativeKeyEvent) {
+        if (nativeKeyEvent.getRawCode() == 20) {
+            bridgeMode = !bridgeMode;
+            System.out.println(bridgeMode);
+        }
     }
 }
