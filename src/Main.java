5ff0896301aafe22bb293d353b3b19a0376de565
Joshua Huang <johuang50@gmail.com>
2020-11-07 20:09:30 -0500

    Import jnative library to hear mouse all system mouse clicks

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,39 @@
+import org.jnativehook.mouse.NativeMouseEvent;
+import org.jnativehook.mouse.NativeMouseInputListener;
+
+import javax.swing.*;
 import java.awt.*;
-import java.awt.event.AWTEventListener;
 
-public class Main {
+public class Main implements NativeMouseInputListener {
     public static void main(String[] args) {
-        Toolkit.getDefaultToolkit().addAWTEventListener(new AWTEventListener() {
-            @Override
-            public void eventDispatched(AWTEvent event) {
+        Toolkit.getDefaultToolkit().addAWTEventListener(event -> System.out.println("MOUSE BUTTON PRESSED"), AWTEvent.MOUSE_EVENT_MASK | AWTEvent.FOCUS_EVENT_MASK);
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
 
-            }
-        }, AWTEvent.MOUSE_EVENT_MASK | AWTEvent.FOCUS_EVENT_MASK);
+    @Override
+    public void nativeMouseClicked(NativeMouseEvent nativeMouseEvent) {
+        
+    }
+
+    @Override
+    public void nativeMousePressed(NativeMouseEvent nativeMouseEvent) {
+
+    }
+
+    @Override
+    public void nativeMouseReleased(NativeMouseEvent nativeMouseEvent) {
+
+    }
+
+    @Override
+    public void nativeMouseMoved(NativeMouseEvent nativeMouseEvent) {
+
+    }
+
+    @Override
+    public void nativeMouseDragged(NativeMouseEvent nativeMouseEvent) {
+
     }
 }
